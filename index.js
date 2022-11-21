102515a2bfd5a08f832152241c11f1c63bb5784e
cutraniramlan <cutraniramlan@gmail.com>
2022-11-21 15:20:53 +0100

    Adding Get, Create, Read, Update, Delete

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require("express");
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 const morgan = require("morgan");
-const app = express();
 
 app.use(morgan("common"));
 app.use(express.static("public"));
 app.use("/documentation.html", express.static("public"));
 (fs = require("fs")), // import built in node modules fs and path
   (path = require("path"));
+app.use(bodyParser.json());
 
 // create a write stream (in append mode)
 // a ‘log.txt’ file is created in root directory
@@ -16,58 +19,212 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 // setup the logger
 app.use(morgan("combined", { stream: accessLogStream }));
 
-let top10Movies = [
+let users = [
+  {
+    id: 1,
+    name: "Kim",
+    favoriteMovies: [],
+  },
+  {
+    id: 2,
+    name: "Caca ",
+    favoriteMovies: [],
+  },
+];
+
+let movies = [
   {
     title: "The Lion King",
-    director: "Roger Allers",
-    genre: "Musical Drama",
+    description: "",
+    genre: {
+      name: "Musical Drama",
+    },
+    director: {
+      name: "Roger Allers",
+      bio: "",
+    },
   },
   {
     title: "Beauty And The Beast",
-    director: "Gary Trousdale",
-    genre: "Musical, Romance, Fantasy",
+    description: "",
+    genre: {
+      name: "Romance",
+    },
+    director: {
+      name: "Gary Trousdale",
+      bio: "",
+    },
   },
+
   {
     title: "The Little Mermaid",
-    director: "John Musker",
-    genre: "Musical, Romance, Fantasy",
+    description: "",
+    genre: {
+      name: "Fantasy",
+    },
+    director: {
+      name: "John Musker",
+      bio: "",
+    },
   },
+
   {
     title: "Mary Poppins",
-    director: "Robert Stevenson",
-    genre: "Musical, Comedy, Fantasy",
+    description: "",
+    genre: {
+      name: "Fantasy",
+    },
+    director: {
+      name: "Robert Stevenson",
+      bio: "",
+    },
   },
+
   {
     title: "Aladdin",
-    director: "John Musker",
-    genre: "Musical, Romance, Fantasy, Adventure",
+    description: "",
+    genre: {
+      name: "Romance",
+    },
+    director: {
+      name: "John Musker",
+      bio: "",
+    },
   },
   {
     title: "cinderella",
-    director: "Clyde Geronimi",
-    genre: "Drama, Romance, Fantasy",
+    description: "",
+    genre: {
+      name: "Romance",
+    },
+    director: {
+      name: "Clyde Geronimi",
+      bio: "",
+    },
   },
+
   {
     title: "Alice in Wonderland",
-    director: "Clyde Geronimi",
-    genre: "Fantasy",
+    description: "",
+    genre: {
+      name: "Fantasy",
+    },
+    director: {
+      name: "Clyde Geronimi",
+      bio: "",
+    },
   },
+
   {
     title: " Pinocchio",
-    director: "Hamilton Luske",
-    genre: "Musical, Adventure, Drama",
+    description: "",
+    genre: {
+      name: "Drama",
+    },
+    director: {
+      name: "Hamilton Luske",
+      bio: "",
+    },
   },
   {
     title: "The Jungle Book",
-    director: "Wolfgang Reitherman",
-    genre: "Mystery, Adventure",
+    description: "",
+    genre: {
+      name: "Mystery",
+    },
+    director: {
+      name: "Wolfgang Reitherman",
+      bio: "",
+    },
   },
+
   {
     title: "Sleeping Beauty",
-    director: "David Hand",
-    genre: "Musical, Romance, Fantasy",
+    description: "",
+    genre: {
+      name: "Romance",
+    },
+    director: {
+      name: "David Hand",
+      bio: "",
+    },
   },
 ];
+
+// CREATE
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names");
+  }
+});
+
+//  UPDATE
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// POST
+app.post("/users/:id/:movieTitle ", (req, res) => {
+  const { id, movietitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movietitle);
+    res.status(200).send(`${movietitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// DELETE
+app.delete("/users/:id/:movieTitle ", (req, res) => {
+  const { id, movietitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movietitle
+    );
+    res
+      .status(200)
+      .send(`${movietitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// DELETE user (deregister )
+app.delete("/users/:id ", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res.json(users);
+    //res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
 
 // GET requests
 app.get("/", (req, res) => {
@@ -78,8 +235,47 @@ app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
 });
 
+// READ
 app.get("/movies", (req, res) => {
-  res.json(top10Movies);
+  res.status(200).json(movies);
+});
+
+// get movies by title
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie");
+  }
+});
+
+// get genre by Name
+app.get("/movies/genre/:genrename", (req, res) => {
+  const { genrename } = req.params;
+  const genre = movies.find((movie) => movie.genre.name === genrename).genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre");
+  }
+});
+
+// get director by name
+app.get("/movies/directors/:directorname", (req, res) => {
+  const { directorname } = req.params;
+  const director = movies.find(
+    (movie) => movie.director.name === directorname
+  ).director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("no such director");
+  }
 });
 
 // ERROR Handling
