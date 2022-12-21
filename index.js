7ece580179a6fe713d191f89ae46d0fbde10b80a
cutraniramlan <cutraniramlan@gmail.com>
2022-12-21 12:37:33 +0100

    update

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/documentation.html", express.static("public"));
   (path = require("path"));
 app.use(bodyParser.json());
 
-/* const cors = require("cors");
+const cors = require("cors");
 let allowedOrigins = [
   "http://localhost:8080",
   "http://testsite.com",
@@ -51,7 +51,7 @@ app.use(
       return callback(null, true);
     },
   })
-); */
+);
 
 const cors = require("cors");
 app.use(cors());
